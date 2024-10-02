35864f66e0a612658dfe1705787b9de5488c860f
machage9603 <mikemachage@gmail.com>
2024-10-02 21:36:19 +0300

    hoisting

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+    let task = false; // Block-scoped
+    let task2 = true; // Block-scoped
+
+    if (trueOrFalse) {
+      task = true;    // Updates the outer `task`
+      task2 = false;  // Updates the outer `task2`
+    }
+
+    return [task, task2];
+  }
