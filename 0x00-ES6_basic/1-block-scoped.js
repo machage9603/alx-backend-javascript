04385c8fcb8ccaa6e0515e67420f4b21370d748e
machage9603 <mikemachage@gmail.com>
2024-10-03 11:19:15 +0300

    solve eslint errors

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false; // Block-scoped
-    let task2 = true; // Block-scoped
+  let task = false; // Block-scoped
+  let task2 = true; // Block-scoped
 
-    if (trueOrFalse) {
-      task = true;    // Updates the outer `task`
-      task2 = false;  // Updates the outer `task2`
-    }
-
-    return [task, task2];
+  if (trueOrFalse) {
+    task = true; // Updates the outer `task`
+    task2 = false; // Updates the outer `task2`
   }
+
+  return [task, task2];
+}
