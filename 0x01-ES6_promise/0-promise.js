095f18372a7ba87e9bcd2259816e58d675046ae5
machage9603 <mikemachage@gmail.com>
2024-10-13 18:50:53 +0300

    solve eslint errors

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 function getResponseFromAPI() {
-    return new Promise((resolve) => {
-        resolve('Response from API');
-    });
+  return new Promise((resolve) => {
+    resolve('Response from API');
+  });
 }
 export default getResponseFromAPI;
