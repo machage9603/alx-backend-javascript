869da4ea8e5a18ee5810c41480440bed77e54a15
machage9603 <mikemachage@gmail.com>
2024-10-14 01:01:16 +0300

    JS promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoData, userData]) => {
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
+export default handleProfileSignup;
---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+function quardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
+export default quardrail;
