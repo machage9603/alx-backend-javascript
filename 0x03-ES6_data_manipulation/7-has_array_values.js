4cb9c9bc60eafcb90b356a34e3dab8a82446b369
machage9603 <mikemachage@gmail.com>
2024-10-20 23:18:51 +0300

    solve eslint errors

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,11 +1,11 @@
 /* eslint-disable array-callback-return */
 export default function hasValuesFromArray(set, list) {
-    let verdict = true;
-    list.map((x) => {
-        if (!set.has(x)) {
-            verdict = false;
-        }
-    });
+  let verdict = true;
+  list.map((x) => {
+    if (!set.has(x)) {
+      verdict = false;
+    }
+  });
 
-    return verdict;
+  return verdict;
 }
